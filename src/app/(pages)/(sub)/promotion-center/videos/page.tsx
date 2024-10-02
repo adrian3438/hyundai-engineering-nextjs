81fe0a831bc9f671e678bdef5c12af007fca8931
adrian3438 <admin@bmcnet.kr>
2024-10-02 10:33:24 +0900

    modify

--- src/app/(pages)/(sub)/promotion-center/videos/page.tsx
+++ src/app/(pages)/(sub)/promotion-center/videos/page.tsx
@@ -6,7 +6,7 @@ import api from "lib/api";
 export default async function PromotionCenterVideos() {
     const response = await api.get(`/user/promotion/getPrVideoList.php?videoType=1&page=1&size=99&keyword=&sortColumn=videoMdate&sortOrder=desc`)
     const data = response?.data?.Result === true ? response?.data?.List : []
-    
+    console.log(data)
     return (
         <Fragment>
             <Videos
